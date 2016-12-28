4f9aeee5569ebcfed454f766d9866601f43f0fe9
simon.vezey <simon.vezey@activus.co.uk>
2016-12-28 14:17:00 +0000

    simple example with full postbacks

--- WebApplication6/Controllers/HomeController.cs
+++ WebApplication6/Controllers/HomeController.cs
@@ -6,25 +6,67 @@ using System.Web.Mvc;
 
 namespace WebApplication6.Controllers
 {
+    using WebApplication6.Models;
+
+    // This isn't how I would normally do a controller but I've kept it as simple as possible
+    // to show how you can get the dropdowns to populate their values
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var model = new PostWholeFormExampleViewModel() { Message = "Hello", People = PeopleDropdownItems(), Pets = PetsDropdownItems(1) };
+            return View(model);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(PostWholeFormExampleViewModel viewModel)
         {
-            ViewBag.Message = "Your application description page.";
+            // Normally you would do some sort of validation to make sure the view
+            // model that's been posted back is valid, i.e. the user hasn't dicked around with it too much.
 
-            return View();
+            // The drop down options aren't posted back so reload them here.
+            viewModel.Pets = PetsDropdownItems(viewModel.SelectedPersonId);
+            viewModel.People = PeopleDropdownItems();
+
+            return View(viewModel);
         }
 
-        public ActionResult Contact()
+        private static List<SelectListItem> PeopleDropdownItems()
         {
-            ViewBag.Message = "Your contact page.";
+            // This would do a proper database lookup or something and probably not be in the controller (seperation of concern and all that)
+            return new List<SelectListItem>
+                       {
+                           new SelectListItem() { Text = "Simon", Value = 1.ToString() },
+                           new SelectListItem() { Text = "Mike", Value = 2.ToString() },
+                           new SelectListItem() { Text = "Emma", Value = 3.ToString() }
+                       };
+        }
 
-            return View();
+        private static List<SelectListItem> PetsDropdownItems(int? selectedPersonId)
+        {
+            // This would do a proper database lookup or something and probably not be in the controller (seperation of concern and all that)
+            var pets = new List<SelectListItem>();
+            if (!selectedPersonId.HasValue) return pets;
+
+            switch (selectedPersonId)
+            {
+                case 1:
+                    pets.Add(new SelectListItem { Text = "Rusty" });
+                    pets.Add(new SelectListItem { Text = "Claws McFrenzy" });
+                    break;
+                case 2:
+                    pets.Add(new SelectListItem { Text = "Pepsi" });
+                    pets.Add(new SelectListItem { Text = "Esmy" });
+                    break;
+                case 3:
+                    pets.Add(new SelectListItem { Text = "Leloo" });
+                    pets.Add(new SelectListItem { Text = "Buddy" });
+                    pets.Add(new SelectListItem { Text = "Fifi" });
+                    break;
+            }
+
+            return pets;
         }
     }
 }
---
+++ WebApplication6/Models/PostWholeFormExampleViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication6.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Web.Mvc;
+
+    public class PostWholeFormExampleViewModel
+    {
+        // I've included this to demonstate that the value isn't lost when you do a post back.
+        [DisplayName("Put a message here to prove that it gets posted back and isn't lost when you re-build the form")]
+        public string Message { get; set; }
+
+        public int? SelectedPersonId { get; set; }
+
+        [DisplayName("Person")]
+        public IList<SelectListItem> People { get; set; }
+
+        public int? SelectedFavouritePetId { get; set; }
+
+        [DisplayName("Favourite pet")]
+        public IList<SelectListItem> Pets { get; set; }
+    }
+}
